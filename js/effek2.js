0a696833744b358d2271d461cea8f0d32d4024ea
unknown <rizqysultan4@gmail.com>
2023-09-28 09:32:36 +0700

    perubahan

--- js/effek2.js
+++
@@ -1,9 +0,0 @@
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-  
